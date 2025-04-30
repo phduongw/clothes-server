2ce57d60a5e53fb40f125b466f5dea525dac6015
DuongpH <eganpham.99@gmail.com>
2025-04-30 17:45:19 +0700

    Testing auth midle

--- test/auth-middleware.test.ts
+++ test/auth-middleware.test.ts
@@ -1,5 +1,6 @@
 import { verifyToken } from '../src/middlewares/jwt';
 import httpMocks from 'node-mocks-http';
+import {BaseResponse} from "../src/controllers/responses/BaseResponse";
 
 describe('auth-middleware', () => {
     it("Authorization Header is empty", () => {
@@ -10,12 +11,17 @@ describe('auth-middleware', () => {
         verifyToken(req, resp, next);
 
         expect(resp.statusCode).toBe(401);
+
+        resp._getJSONData()
+        // expect(resp.status.message)
     });
 
     it('Access Token is not valid', () => {
         const req = httpMocks.createRequest();
         const resp = httpMocks.createResponse();
         const next = jest.fn();
+
+
     });
 
 
